7df0db41a02599310ecd23bce831195bd1f89329
Ricardo Canastro <ricardo.canastro@blip.pt>
2015-08-15 22:23:39 +0100

    Refactored into flux architecture

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,48 @@
 var React = require('react');
 
-var App = React.createClass({
+var playlist = [{
+    url: '1',
+    votes: 0
+}, {
+    url: '2',
+    votes: 3
+}, {
+    url: '3',
+    votes: -1
+},];
+
+
+var Card = React.createClass({
     render() {
-        return <h1>Hello World</h1>;
+        return (
+            <div>
+                <h1>{this.props.url}</h1>
+                <h3>{this.props.votes}</h3>
+                <button onClick={this.props.onClick}>Play</button>
+            </div>
+        );
     }
 });
 
-React.render(<App/>, document.getElementById('example'));
+var Playlist = React.createClass({
+
+    play: function (item) {
+        alert(item.url);
+    },
+
+    render() {
+        var that = this;
+
+        return (
+            <div>
+                {this.props.playlist.map(function (item) {
+                    return (
+                        <Card onClick={that.play.bind(null, item)} url={item.url} votes={item.votes}></Card>
+                    )
+                })}
+            </div>
+        );
+    }
+})
+
+React.render(<Playlist playlist={playlist}/>, document.getElementById('playlist'));
