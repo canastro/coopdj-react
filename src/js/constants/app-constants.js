9030daef82c1f2630d65c25e2bef3be2487d9686
Ricardo Canastro <ricardo.canastro@blip.pt>
2015-08-16 15:39:12 +0100

    Added youtube thumbnail url to constants

--- src/js/constants/app-constants.js
+++ src/js/constants/app-constants.js
@@ -2,5 +2,6 @@ module.exports = {
     PLAY: 'PLAY',
     VOTE_UP: 'VOTE_UP',
     VOTE_DOWN: 'VOTE_DOWN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    YOUTUBE_THUMBNAIL_URL: 'http://img.youtube.com/vi/{{VIDEO_ID}}/1.jpg'
 };
