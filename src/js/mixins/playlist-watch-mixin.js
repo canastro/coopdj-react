50a4e5add0de53a5efb3556fa28a2ad7a9a7f3b7
Ricardo Canastro <ricardo.canastro@blip.pt>
2015-08-16 15:38:17 +0100

    Added Playlist Watch mixin

---
+++ src/js/mixins/playlist-watch-mixin.js
@@ -0,0 +1,22 @@
+/** @jsx React.DOM */
+var React = require('react');
+var AppStore = require('../stores/app-store');
+
+var PlaylistWatchMixin = function (cb) {
+    return {
+        getInitialState: function () {
+            return cb();
+        },
+        componentWillMount:function(){
+            AppStore.addChangeListener(this._onChange);
+        },
+        componentWillUnmount:function(){
+            AppStore.removeChangeListener(this._onChange);
+        },
+        _onChange: function(){
+            this.setState(cb());
+        }
+    }
+};
+
+module.exports = PlaylistWatchMixin;
